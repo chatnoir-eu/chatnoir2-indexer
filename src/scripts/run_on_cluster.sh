a99f71586a82fb06d78a22359238103ab7837f9d
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2020-12-02 13:17:50 +0100

    Update cluster script

--- src/scripts/run_on_cluster.sh
+++ src/scripts/run_on_cluster.sh
@@ -2,10 +2,12 @@
 
 corpus="$1"
 dataset="$2"
-index_revision="$3"
-batches="$4"
-partitions="$5"
+index_name="$3"
+partitions="$4"
+batches="$5"
+keystore="$6"
 
+set -e
 trap "echo Exiting on request >&2; exit" SIGINT SIGTERM
 
 if [[ "$corpus" != "cw" ]] && [[ "$corpus" != "cc" ]]; then
@@ -13,22 +15,27 @@ if [[ "$corpus" != "cw" ]] && [[ "$corpus" != "cc" ]]; then
     exit 1
 fi
 
-if [[ "$dataset" == "" ]]; then
+if [ -z "$dataset" ]; then
     echo "You need to specify a source dataset (09, 12, CC-MAIN-20...)." >&2
     exit 1
 fi
 
-if [[ "$index_revision" == "" ]]; then
-    echo "You need to specify an index revision." >&2
+if [ -z "$index_name" ]; then
+    echo "You need to specify a target index name." >&2
     exit 1
 fi
 
-if [[ "$partitions" == "" ]]; then
-    echo "You need to specify the number of partitions." >&2
+if [ -z "$partitions" ]; then
+    echo "You need to specify the number of MapFile partitions." >&2
     exit 1
 fi
 
-if [[ "$batches" == "" ]]; then
+if [ -z "$batches" ]; then
+    echo "You need to specify the number of indexing batches." >&2
+    exit 1
+fi
+
+if [ -z "$batches" ]; then
     echo "You need to specify the number of indexing batches." >&2
     exit 1
 fi
@@ -42,43 +49,53 @@ for batch in $(seq 1 ${batches}); do
     if [[ "$corpus" == "cw" ]] && [[ "$dataset" == "09" ]]; then
         hadoop jar $(dirname $0)/../../build/libs/chatnoir2-indexer-*-all.jar \
             "de.webis.chatnoir2.indexer.app.ChatNoirIndexer" \
-            -Des.nodes=betaweb015,betaweb016,betaweb017,betaweb018,betaweb019 \
+            -Des.nodes=betaweb015.bw.webis.de,betaweb035.bw.webis.de,betaweb055.bw.webis.de,betaweb075.bw.webis.de,betaweb095.bw.webis.de,betaweb115.bw.webis.de \
+            -Des.port=30920 \
+            -Des.net.ssl=true \
+            -Des.batch.size.entries=700 \
+            -Des.batch.size.bytes=3mb \
+            $@ \
             -uuid-prefix "clueweb${dataset}" \
-            -sequence-files "/corpora/clueweb/${dataset}-mapfile/" \
-            -spamranks "/corpora/clueweb/${dataset}-spam-rankings/*" \
-            -anchortexts "/corpora/clueweb/${dataset}-anchors/*" \
-            -pageranks "/corpora/clueweb/${dataset}-page-ranks.txt" \
+            -sequence-files "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-mapfile/" \
+            -partitions ${partitions} \
+            -spamranks "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-spam-rankings/*" \
+            -anchortexts "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-anchors/*" \
+            -pageranks "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-page-ranks.txt" \
             -batches ${batches} \
             -batch-num ${batch} \
-            -index "webis_warc_clueweb${dataset}_${index_revision}" $@
+            -index "${index_name}"
     elif [[ "$corpus" == "cw" ]] && [[ "$dataset" == "12" ]]; then
         hadoop jar $(dirname $0)/../../build/libs/chatnoir2-indexer-*-all.jar \
             "de.webis.chatnoir2.indexer.app.ChatNoirIndexer" \
-            -Des.nodes=betaweb015,betaweb016,betaweb017,betaweb018,betaweb019 \
+            -Des.nodes=betaweb015.bw.webis.de,betaweb035.bw.webis.de,betaweb055.bw.webis.de,betaweb075.bw.webis.de,betaweb095.bw.webis.de,betaweb115.bw.webis.de \
+            -Des.port=30920 \
+            -Des.net.ssl=true \
+            -Des.batch.size.entries=700 \
+            -Des.batch.size.bytes=3mb \
+            $@ \
             -uuid-prefix "clueweb${dataset}" \
-            -sequence-files "/corpora/clueweb/${dataset}-mapfile/" \
-            -spamranks "/corpora/clueweb/${dataset}-spam-rankings/*" \
-            -pageranks "/corpora/clueweb/${dataset}-page-ranks.txt.bz2" \
+            -sequence-files "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-mapfile/" \
+            -partitions ${partitions} \
+            -spamranks "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-spam-rankings/*" \
+            -pageranks "/corpora/corpora-thirdparty/corpus-clueweb/${dataset}-page-ranks.txt.bz2" \
             -batches ${batches} \
             -batch-num ${batch} \
-            -index "webis_warc_clueweb${dataset}_${index_revision}" $@
+            -index "${index_name}"
     elif [[ "$corpus" == "cc" ]]; then
-        index_name="$(echo ${dataset} | awk '{print tolower($0)}' | sed s/cc-main-20// | sed s/-//g)"
-
         hadoop jar $(dirname $0)/../../build/libs/chatnoir2-indexer-*-all.jar \
             "de.webis.chatnoir2.indexer.app.ChatNoirIndexer" \
-            -Des.nodes=betaweb015,betaweb016,betaweb017,betaweb018,betaweb019 \
-            -Des.batch.size.entries=7000 \
-            -Des.batch.size.bytes=10mb \
+            -Des.nodes=betaweb015.bw.webis.de,betaweb035.bw.webis.de,betaweb055.bw.webis.de,betaweb075.bw.webis.de,betaweb095.bw.webis.de,betaweb115.bw.webis.de \
+            -Des.port=30920 \
+            -Des.net.ssl=true \
+            -Des.batch.size.entries=700 \
+            -Des.batch.size.bytes=3mb
             -Dmapreduce.job.split.metainfo.maxsize=-1 \
+            $@ \
             -uuid-prefix "commoncrawl" \
             -sequence-files "/corpora/corpus-commoncrawl/${dataset}-mapfile/" \
             -partitions ${partitions} \
             -batches ${batches} \
             -batch-num ${batch} \
-            -index "webis_warc_commoncrawl${index_name}_${index_revision}" $@
-
-        #echo "Merging down segments..."
-        #curl -XPOST "http://betaweb015:9200/webis_warc_commoncrawl15_${index_revision}/_forcemerge?max_num_segments=1"
+            -index "${index_name}"
     fi
 done
